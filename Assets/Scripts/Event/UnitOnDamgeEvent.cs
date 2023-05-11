e1d0eb98745dc84626fa046ea03f59163d319269
IamBingBing <03lonnie@naver.com>
2023-05-11 23:02:37 +0900

    Add files via upload

--- Assets/Scripts/Event/UnitOnDamgeEvent.cs
+++ Assets/Scripts/Event/UnitOnDamgeEvent.cs
@@ -3,11 +3,30 @@ using System.Collections.Generic;
 using UnityEngine.Events;
 
 public class UnitOnDamgeEvent : MonoBehaviour 
-{
+{   
+    private :
+        int damge;
+        BaseUnit attacker;
+        BaseUnit defender;
     UnityEvent event ; 
-    void Start() { 
-        if (event == null) {
-            event = new UnityEvent();
+    public:
+        UnitOnDamgeEvent ( int a , BaseUnit attack , BaseUnit defend) {
+            damge = a;
+            attacker = attack
+            defender = defend
         }
-    }
+        void Start() { 
+            if (event == null) {
+                event = new UnityEvent();
+        }
+        }
+        void getDamge() {
+            return damge;
+        }
+        void getAttacker (){
+            return attacker;
+        }
+        void getDefender () { 
+            return defender;
+        }
 }
