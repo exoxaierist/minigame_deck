fd76804c741948ed8185b77668eff02372363714
highlow12 <77542378+highlow12@users.noreply.github.com>
2023-05-15 23:25:14 +0900

    일정시간동안 데미지 받지 않있을 때 주석달음
    DamageTimeout으로 이름 변경, 주석 작성
    이후에 할 것: '턴 시작시' 델리게이트 만들고 그것 기반으로 코드 수정하기

--- Assets/Scripts/System/JunSystem/DamageTimeout.cs
+++ Assets/Scripts/System/JunSystem/DamageTimeout.cs
@@ -3,13 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TimeNoDamage : UnitBase
+public class DamageTimeout : UnitBase
 {
+    [Tooltip("TimeoutTurn 동안 데미지를 입지 않으면 발동")]
     public int TimeoutTurn = 1;
+
+    //prevTurn - deltaTurn == TimeoutTurn 일때 함수 발동
+    //prevTurn: 이전 턴
+    //deltaTurn: 마지막 함수 발동 이후로 지난 턴
+
     private int deltaTurn = 0;
     private int prevTurn = 0;
 
-    protected virtual void OnNoDamageTimeout() { }
+    //데미지를 받지 않았을때 발동
+    protected virtual void OnNoDamage() { }
     void ResetTimer()
     {
         deltaTurn = 0;
@@ -18,7 +25,7 @@ public class TimeNoDamage : UnitBase
     protected override void Awake()
     {
         base.Awake();
-        hp.OnDamage += ResetTimer;
+        hp.OnDamage += ResetTimer;//데미지를 입는 경우에 타이머 리셋
     }
     protected void Update()
     {
@@ -31,9 +38,9 @@ public class TimeNoDamage : UnitBase
         }
         if (deltaTurn == TimeoutTurn)
         {
-            OnNoDamageTimeout();
+            OnNoDamage();
             Debug.Log("nodamage");
-            deltaTurn = 0;
+            deltaTurn = 0; //함수 발동시 타이머 리셋
         }
 
     }
