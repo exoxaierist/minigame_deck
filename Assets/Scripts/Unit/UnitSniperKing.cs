a607032b1bc18c8a9d335856356039f31d5bb69b
highlow12 <77542378+highlow12@users.noreply.github.com>
2023-06-29 23:17:23 +0900

    스나이퍼킹 완성
    완성

--- Assets/Scripts/Unit/UnitSniperKing.cs
+++ Assets/Scripts/Unit/UnitSniperKing.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class UnitSniperKing : UnitBase
 {
-    // Start is called before the first frame update
-    void Start()
+    int damageAdd = 0;
+    public override void Kill()
     {
-        
+        damageAdd++;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Attack()
     {
+        if (!matchMode || turnCount <= 0) return;
+        turnCount--;
+        
+        AttackInfo info = new() //공격 정보
+        {
+            damage = 1 + damageAdd,
+            attacker = this
+        };
+        for (int i = 0; i < 6; i++)
+        {
+            Vector2 target = transform.position * Vector2.one + lastMoveDir * i; //공격할 위치
+            Global.atkPooler.Get().Attack(target, info); //공격
+        }
         
     }
 }
